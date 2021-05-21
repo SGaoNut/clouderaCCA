51ea6060b638dda7bf3c31db9a3b33afb51b4740
shan <gs0627@icloud.com>
2021-05-21 17:38:38 +0800

    online

--- src/main/scala/com/shan/q2Solu.scala
+++ src/main/scala/com/shan/q2Solu.scala
@@ -1,13 +1,15 @@
 package com.shan
 
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{Row, SparkSession}
+//import com.databricks.spark.avro._
 
 object q2Solu {
   def main(args: Array[String]): Unit = {
-    if (args.length < 1) {
-      System.err.print("Usage: com.shan.q2Solu <logfile>")
-      System.exit(1)
-    }
+//    if (args.length < 1) {
+//      System.err.print("Usage: com.shan.q2Solu <logfile>")
+//      System.exit(1)
+//    }
 
     //TODO: complete exercise
     //1. 构建SparkConf对象，设置application的名称
@@ -16,11 +18,16 @@ object q2Solu {
     val sc = new SparkContext(sparkConf)
     sc.setLogLevel("WARN")
 
-    val acct = sc.textFile("/loudacre/accounts")
-    val wlog = sc.textFile("loudacre/weblogs")
+    val acct = sc.textFile("hdfs://localhost:8020/loudacre/accounts")
+    val wlog = sc.textFile("hdfs://localhost:8020/loudacre/weblogs")
     val acct1 = acct.map(_.split(",")).map(ar => (ar(0), (ar(3), ar(4))))
-    val wlog1 = wlog.map(_.split(" "))
-
-
+    val wlog1 = wlog.map(_.split(" ")).map(ar => (ar(2), (ar(0))))
+    val acct_wlog = acct1.join(wlog1).map{case (id,((fname, lname),ip_addr)) => (ip_addr,lname,fname)}
+    val sparkSession = SparkSession.builder().appName("q2Solu").master("local").enableHiveSupport().getOrCreate()
+    import sparkSession.implicits._
+    val res = acct_wlog.toDF("ip_addr,lname,fname")
+    res.write.orc("hdfs://localhost:8020/loudacre/problem2/solution/orc")
+//    res.write.avro("hdfs://localhost:8020/loudacre/problem2/solution/avro")
 
   }
+}
